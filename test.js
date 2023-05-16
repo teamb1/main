cd745c66b8dab7fb03b8fecf731f45573b5e9c3e
Vinetuoj <116194041+Vinetuoj@users.noreply.github.com>
2023-05-16 19:05:05 +0200

    kk

--- test.js
+++
@@ -1,6 +0,0 @@
- /////// uložení mapy /////
-let map = document.querySelector("#map")
-
-///// teplota //////////
-let tempature = src="https://api.open-meteo.com/v1/forecast?latitude=49.20&longitude=16.61&hourly=temperature_2m"
-console.log(tempature)
